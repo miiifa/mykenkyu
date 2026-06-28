abbe524270bb0322dc2a8f6027b53658d66f62cd
miiifa <86800867+miiifa@users.noreply.github.com>
2026-06-28 12:38:47 +0900

    Prepare controller for research demo

--- app/controllers/god_controller.rb
+++ app/controllers/god_controller.rb
@@ -1,16 +1,14 @@
 class GodController < ApplicationController
-    protect_from_forgery
-    def index
-        
-        if request.post? then
-            @title= 'Result'
-            @msg= 'you typed: '+ params['input1'] +'.'
-            @value= params['input1']
-        else
-            @title= 'Index'
-            @msg= 'type text ...'
-            @value= '' 
-        end    
-    end
-    
+  protect_from_forgery
+
+  def index
+    @title = 'Puzzle Research Web Demo'
+    @subtitle = '修士研究のパズル生成・描画処理をWeb上で体験するためのMVPです。'
+    @research_points = [
+      '入力画像を格子状のセルに分割する',
+      'セルをシャッフルしてパズルとして描画する',
+      '元画像の配置に戻すことで復元イメージを確認する',
+      '将来的にはshu-ron側の距離計算・対応付けアルゴリズムと接続する'
+    ]
+  end
 end
